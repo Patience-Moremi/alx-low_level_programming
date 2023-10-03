9faadbfcea654155f5971c02c27b944078ed2ffb
Patience-Moremi <phetogopatience@gmail.com>
2023-10-03 10:43:14 -0700

    upate

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,30 +1,32 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
- * _strdup - allocates space in memory
- * @str: input string
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: the source string
  *
- * Return: returns a pointer to a newly
- * allocated space in memory
+ * Return: returns a pointer to the duplicated string.
+ * It returns NULL if insufficient memory was available
  */
-
 char *_strdup(char *str)
 {
-	char *duplicate_num;
-	int i = 0, len = 0;
+	char *copy;
+	int i, len = 0;
 
-	duplicate_num = malloc(sizeof(char) * (len + 1));
-	if (str == 0 || duplicate_num == NULL)
-	{
+	if (str == NULL)
 		return (NULL);
-	}
-	for (i = 0; str[i]; i++)
-	{
-		duplicate_num[i] = str[i];
+
+	while (str[len] != '\0')
 		len++;
-	}
-	duplicate_num[len] = '\0';
-	return (duplicate_num);
-}}
+
+	copy = (char *)malloc((sizeof(char) * len) + 1);
+	if (copy == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		copy[i] = str[i];
+	copy[len] = '\0';
+
+	return (copy);
+}
--- 0x0B-malloc_free/4-free_grid.c4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c4-free_grid.c
@@ -1,23 +1,16 @@
-#include "holberton.h"
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 
 /**
  * free_grid - frees a 2 dimensional grid
- * @grid: the address of the two dimensional grid
+ * @grid: the grid
  * @height: height of the grid
- *
- * Description: print_chessboard
- * Return: Nothing.
  */
-
 void free_grid(int **grid, int height)
 {
 	int i;
 
 	for (i = 0; i < height; i++)
-	{
 		free(grid[i]);
-	}
 	free(grid);
 }
